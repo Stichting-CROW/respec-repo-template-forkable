318d4ab766e455b4301b980b4870ef58b38fd6bc
redmer <redmer@users.noreply.github.com>
2022-11-09 20:27:51 +0000

    Initialize new ReSpec repository
    Reason: run setup.yaml after clone by redmer

---
+++ docs/respec-repo-template-forkable/config.js
@@ -0,0 +1,15 @@
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specStatus: "DRAFT",
+  shortName: "respec-repo-template-forkable/respec-repo-template-forkable",
+  specType: "technische documentatie", // vrije tekst, kleine letter
+  imprint: "crow",
+  editors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
+    {
+      name: "Redmer Kronemeijer",
+    },
+  ],
+  github: "https://github.com/Stichting-CROW/respec-repo-template-forkable",
+  license: "undefined",
+};
